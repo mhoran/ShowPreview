2f6d401c280c884b40316e8e597d84106b3b2362
Matthew Horan <matt@matthoran.com>
2010-10-16 15:06:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowPlannr::Application.config.secret_token = '364a35e42ac19de33a9024cda767d51ec77f5ec86e5417e3bec13382d23d0653552aaa98a942103ad732f1b4e624d06e569b611dd1e237f2e6bf80c2d013b570'
