ae3a21bad7c058e3ffc7229bd81c8f41eeb6e096
Matthew Horan <matt@matthoran.com>
2010-10-17 14:04:37 -0400

    Fix Show.all

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,11 +5,15 @@ class Show
   format :json
 
   def self.all(artist_name, latitude, longitude, page = 1)
-    q = { :artist_name => artist_name, :location => "geo:#{latitude},#{longitude}", :page => page }
+    q = { :artist_name => artist_name, :location => "geo:#{latitude},#{longitude}", :page => page, :per_page => 1 }
     res = self.get('/api/3.0/events.json', :query => q)['resultsPage']
-    r = []
-    if res && res['results'] && page.pred * res['perPage'] <= res['totalEntries']
-      r = res['results']['event'] | self.all(artist_name, latitude, longitude, page.next)
+    if res && res['results']
+      r = res['results']['event'] 
+      if page * res['perPage'] < res['totalEntries']
+        r += self.all(artist_name, latitude, longitude, page.next)
+      end
+    else
+      r = []
     end
     r
   end
